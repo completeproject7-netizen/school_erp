1186f610c4026436f82340536b0d84e40188f912
Pratyush Sharma <pratyush@example.com>
2026-05-26 12:17:31 +0530

    fix express wildcard route

--- server/server.js
+++ server/server.js
@@ -97,7 +97,7 @@ app.use("/api", contentRoutes);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
